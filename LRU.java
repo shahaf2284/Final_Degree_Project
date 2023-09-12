83afbd094820052f24cd33ea4ed6b7b8679555b2
shahaf2284 <122786017+shahaf2284@users.noreply.github.com>
2023-09-12 18:42:15 +0300

    Create LRU.java

---
+++ LRU.java
@@ -0,0 +1,34 @@
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class LRUCache<K, V> extends LinkedHashMap<K, V> {
+    private final int capacity;
+
+    public LRUCache(int capacity) {
+        super(capacity, 0.75f, true);
+        this.capacity = capacity;
+    }
+
+    @Override
+    protected boolean removeEldestEntry(Map.Entry<K, V> eldest) {
+        return size() > capacity;
+    }
+
+    public static void main(String[] args) {
+        LRUCache<Integer, String> cache = new LRUCache<>(3);
+
+        cache.put(1, "One");
+        cache.put(2, "Two");
+        cache.put(3, "Three");
+
+        System.out.println(cache); // Output: {1=One, 2=Two, 3=Three}
+
+        cache.get(2); // Accessing 2, making it the most recently used
+
+        System.out.println(cache); // Output: {1=One, 3=Three, 2=Two}
+
+        cache.put(4, "Four"); // Adding a new item, evicting the least recently used (1)
+
+        System.out.println(cache); // Output: {3=Three, 2=Two, 4=Four}
+    }
+}
